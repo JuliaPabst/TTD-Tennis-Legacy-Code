de24fc30d648840a42ef3ff0aa1b9fb5e6f4efff
Jules <testadmin@Julias-MBP-2.home>
2024-06-13 14:59:36 +0200

    add first successful test Initial score is Love-All

--- javascript/__tests__/TennisGame1.test.js
+++ javascript/__tests__/TennisGame1.test.js
@@ -1,3 +1,7 @@
 const TennisGame1 = require('../TennisGame1');
 
+test('Initial score is Love-All', () => {
+  const game = new TennisGame1('player1', 'player2');
+  expect(game.getScore()).toBe('Love-All');
+});
 
