eeabee2a6c956bc0df58a4c9d61944223811dd48
Jules <testadmin@Julias-MBP-2.home>
2024-06-13 14:58:21 +0200

    properly add jest

--- javascript/jest.config.js
+++ javascript/jest.config.js
@@ -1,4 +1,10 @@
 module.exports = {
     testEnvironment: 'node',
+    roots: ['<rootDir>/__tests__'],
+    transform: {
+      '^.+\\.jsx?$': 'babel-jest',
+    },
+    testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.jsx?$',
+    moduleFileExtensions: ['js', 'jsx', 'json', 'node'],
   };
   
